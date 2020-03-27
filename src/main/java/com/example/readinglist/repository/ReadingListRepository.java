c553b72f7be7d82d416fb1fe7f16d43dedf19407
kamalnuriyev <kemal.nuriyev@gmail.com>
2020-03-27 10:40:19 +0400

    Created repository interface which extends JpaRepository interface

---
+++ src/main/java/com/example/readinglist/repository/ReadingListRepository.java
@@ -0,0 +1,11 @@
+package com.example.readinglist.repository;
+
+import com.example.readinglist.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ReadingListRepository extends JpaRepository<Book, Long> {
+
+    List<Book> findByReader(String reader);
+}
