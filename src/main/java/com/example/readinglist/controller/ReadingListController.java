c2631980c3e8b787f29de1f8a1e662bb77907ab9
kamalnuriyev <kemal.nuriyev@gmail.com>
2020-03-30 15:53:19 +0400

    .Added property amazon.associate-id .Created AmazonProperties class to get the related property from application.properties

--- src/main/java/com/example/readinglist/controller/ReadingListController.java
+++ src/main/java/com/example/readinglist/controller/ReadingListController.java
@@ -1,22 +1,27 @@
 package com.example.readinglist.controller;
 
 import com.example.readinglist.model.Book;
+import com.example.readinglist.properties.AmazonProperties;
 import com.example.readinglist.repository.ReadingListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
 @Controller
 public class ReadingListController {
 
+    private AmazonProperties amazonProperties;
     private ReadingListRepository repository;
 
     @Autowired
-    public ReadingListController(ReadingListRepository repository) {
+    public ReadingListController(ReadingListRepository repository, AmazonProperties amazonProperties) {
         this.repository = repository;
+        this.amazonProperties = amazonProperties;
     }
 
     @GetMapping("/reading-list/{reader}")
@@ -24,8 +29,11 @@ public class ReadingListController {
 
         List<Book> readingList = repository.findByReader(reader);
 
-        if (readingList != null)
+        if (readingList != null) {
             model.addAttribute("books", readingList);
+            model.addAttribute("reader", reader);
+            model.addAttribute("associateID", amazonProperties.getAssociateId());
+        }
 
         // logical view name - "readingList"
         return "readingList";
